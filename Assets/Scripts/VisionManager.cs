c4ff968f9ce23ea8552c91ff4f0d0a71d42f21c2
Bruno <brunofvalli@gmail.com>
2018-10-03 09:34:51 -0500

    Removed more files to be checked in.
    Fixed C# code

--- Assets/Scripts/VisionManager.cs
+++ Assets/Scripts/VisionManager.cs
@@ -5,110 +5,123 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.Networking;
 
-namespace Assets.Scripts
+public class VisionManager : MonoBehaviour
 {
-    public class VisionManager : MonoBehaviour
+    #region public classes
+    [System.Serializable]
+    public class TagData
     {
-        [System.Serializable]
-        public class TagData
+        public string name;
+        public float confidence;
+    }
+
+    [System.Serializable]
+    public class AnalysedObject
+    {
+        public TagData[] tags;
+        public string requestId;
+        public object metadata;
+    }
+    #endregion
+
+    #region static public field
+    public static VisionManager instance;
+    #endregion
+
+    #region private fields
+    // you must insert your service key here!    
+    private string authorizationKey = "d7a9531eeb624802bafae9e9f4fcd1bd";
+    private const string ocpApimSubscriptionKeyHeader = "df6ed739-23f7-4c0f-8020-ec0f101d9445";
+    private string visionAnalysisEndpoint = "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Tags";   // This is where you need to update your endpoint, if you set your location to something other than west-us.
+    #endregion
+
+    #region internal fields
+    internal byte[] imageBytes;
+
+    internal string imagePath;
+    #endregion
+
+    #region events
+    private void Awake()
+    {
+        // allows this instance to behave like a singleton
+        instance = this;
+    }
+    #endregion
+
+    #region private methods
+    /// <summary>
+    /// Returns the contents of the specified file as a byte array.
+    /// </summary>
+    private static byte[] GetImageAsByteArray(string imageFilePath)
+    {
+        FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+        BinaryReader binaryReader = new BinaryReader(fileStream);
+        return binaryReader.ReadBytes((int)fileStream.Length);
+    }
+    #endregion
+
+    #region public methods
+    /// <summary>
+    /// Call the Computer Vision Service to submit the image.
+    /// </summary>
+    public IEnumerator AnalyseLastImageCaptured()
+    {
+        WWWForm webForm = new WWWForm();
+        using (UnityWebRequest unityWebRequest = UnityWebRequest.Post(visionAnalysisEndpoint, webForm))
         {
-            public string name;
-            public float confidence;
-        }
+            // gets a byte array out of the saved image
+            imageBytes = GetImageAsByteArray(imagePath);
+            unityWebRequest.SetRequestHeader("Content-Type", "application/octet-stream");
+            unityWebRequest.SetRequestHeader(ocpApimSubscriptionKeyHeader, authorizationKey);
 
-        [System.Serializable]
-        public class AnalysedObject
-        {
-            public TagData[] tags;
-            public string requestId;
-            public object metadata;
-        }
+            // the download handler will help receiving the analysis from Azure
+            unityWebRequest.downloadHandler = new DownloadHandlerBuffer();
 
-        public static VisionManager instance;
+            // the upload handler will help uploading the byte array with the request
+            unityWebRequest.uploadHandler = new UploadHandlerRaw(imageBytes);
+            unityWebRequest.uploadHandler.contentType = "application/octet-stream";
 
-        // you must insert your service key here!    
-        private string authorizationKey = "d7a9531eeb624802bafae9e9f4fcd1bd";
-        private const string ocpApimSubscriptionKeyHeader = "df6ed739-23f7-4c0f-8020-ec0f101d9445";
-        private string visionAnalysisEndpoint = "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Tags";   // This is where you need to update your endpoint, if you set your location to something other than west-us.
+            yield return unityWebRequest.SendWebRequest();
 
-        internal byte[] imageBytes;
+            long responseCode = unityWebRequest.responseCode;
 
-        internal string imagePath;
-
-        private void Awake()
-        {
-            // allows this instance to behave like a singleton
-            instance = this;
-        }
-
-        /// <summary>
-        /// Call the Computer Vision Service to submit the image.
-        /// </summary>
-        public IEnumerator AnalyseLastImageCaptured()
-        {
-            WWWForm webForm = new WWWForm();
-            using (UnityWebRequest unityWebRequest = UnityWebRequest.Post(visionAnalysisEndpoint, webForm))
+            try
             {
-                // gets a byte array out of the saved image
-                imageBytes = GetImageAsByteArray(imagePath);
-                unityWebRequest.SetRequestHeader("Content-Type", "application/octet-stream");
-                unityWebRequest.SetRequestHeader(ocpApimSubscriptionKeyHeader, authorizationKey);
+                string jsonResponse = null;
+                jsonResponse = unityWebRequest.downloadHandler.text;
 
-                // the download handler will help receiving the analysis from Azure
-                unityWebRequest.downloadHandler = new DownloadHandlerBuffer();
+                // The response will be in Json format
+                // therefore it needs to be deserialized into the classes AnalysedObject and TagData
+                AnalysedObject analysedObject = new AnalysedObject();
+                analysedObject = JsonUtility.FromJson<AnalysedObject>(jsonResponse);
 
-                // the upload handler will help uploading the byte array with the request
-                unityWebRequest.uploadHandler = new UploadHandlerRaw(imageBytes);
-                unityWebRequest.uploadHandler.contentType = "application/octet-stream";
-
-                yield return unityWebRequest.SendWebRequest();
-
-                long responseCode = unityWebRequest.responseCode;
-
-                try
+                if (analysedObject.tags == null)
                 {
-                    string jsonResponse = null;
-                    jsonResponse = unityWebRequest.downloadHandler.text;
-
-                    // The response will be in Json format
-                    // therefore it needs to be deserialized into the classes AnalysedObject and TagData
-                    AnalysedObject analysedObject = new AnalysedObject();
-                    analysedObject = JsonUtility.FromJson<AnalysedObject>(jsonResponse);
-
-                    if (analysedObject.tags == null)
-                    {
-                        Debug.Log("analysedObject.tagData is null");
-                    }
-                    else
-                    {
-                        Dictionary<string, float> tagsDictionary = new Dictionary<string, float>();
-
-                        foreach (TagData td in analysedObject.tags)
-                        {
-                            TagData tag = td as TagData;
-                            tagsDictionary.Add(tag.name, tag.confidence);
-                        }
-
-                        ResultsLabel.instance.SetTagsToLastLabel(tagsDictionary);
-                    }
+                    Debug.Log("analysedObject.tagData is null");
                 }
-                catch (Exception exception)
+                else
                 {
-                    Debug.Log("Json exception.Message: " + exception.Message);
-                }
+                    Dictionary<string, float> tagsDictionary = new Dictionary<string, float>();
 
-                yield return null;
+                    foreach (TagData td in analysedObject.tags)
+                    {
+                        TagData tag = td as TagData;
+                        tagsDictionary.Add(tag.name, tag.confidence);
+                    }
+
+                    ResultsLabel.instance.SetTagsToLastLabel(tagsDictionary);
+                }
+            }
+            catch (Exception exception)
+            {
+                Debug.Log("Json exception.Message: " + exception.Message);
             }
 
-            /// <summary>
-            /// Returns the contents of the specified file as a byte array.
-            /// </summary>
-            private static byte[] GetImageAsByteArray(string imageFilePath)
-            {
-                FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
-                BinaryReader binaryReader = new BinaryReader(fileStream);
-                return binaryReader.ReadBytes((int)fileStream.Length);
-            }
+            yield return null;
         }
     }
+    #endregion
+
+
 }
