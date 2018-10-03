29a0d49ccf66f30258f330ea8c9c596a85d276cc
Bruno <brunofvalli@gmail.com>
2018-10-03 09:13:18 -0500

    Added the Vision Manager code

--- Assets/Scripts/VisionManager.cs
+++ Assets/Scripts/VisionManager.cs
@@ -23,5 +23,92 @@ namespace Assets.Scripts
             public string requestId;
             public object metadata;
         }
+
+        public static VisionManager instance;
+
+        // you must insert your service key here!    
+        private string authorizationKey = "d7a9531eeb624802bafae9e9f4fcd1bd";
+        private const string ocpApimSubscriptionKeyHeader = "df6ed739-23f7-4c0f-8020-ec0f101d9445";
+        private string visionAnalysisEndpoint = "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Tags";   // This is where you need to update your endpoint, if you set your location to something other than west-us.
+
+        internal byte[] imageBytes;
+
+        internal string imagePath;
+
+        private void Awake()
+        {
+            // allows this instance to behave like a singleton
+            instance = this;
+        }
+
+        /// <summary>
+        /// Call the Computer Vision Service to submit the image.
+        /// </summary>
+        public IEnumerator AnalyseLastImageCaptured()
+        {
+            WWWForm webForm = new WWWForm();
+            using (UnityWebRequest unityWebRequest = UnityWebRequest.Post(visionAnalysisEndpoint, webForm))
+            {
+                // gets a byte array out of the saved image
+                imageBytes = GetImageAsByteArray(imagePath);
+                unityWebRequest.SetRequestHeader("Content-Type", "application/octet-stream");
+                unityWebRequest.SetRequestHeader(ocpApimSubscriptionKeyHeader, authorizationKey);
+
+                // the download handler will help receiving the analysis from Azure
+                unityWebRequest.downloadHandler = new DownloadHandlerBuffer();
+
+                // the upload handler will help uploading the byte array with the request
+                unityWebRequest.uploadHandler = new UploadHandlerRaw(imageBytes);
+                unityWebRequest.uploadHandler.contentType = "application/octet-stream";
+
+                yield return unityWebRequest.SendWebRequest();
+
+                long responseCode = unityWebRequest.responseCode;
+
+                try
+                {
+                    string jsonResponse = null;
+                    jsonResponse = unityWebRequest.downloadHandler.text;
+
+                    // The response will be in Json format
+                    // therefore it needs to be deserialized into the classes AnalysedObject and TagData
+                    AnalysedObject analysedObject = new AnalysedObject();
+                    analysedObject = JsonUtility.FromJson<AnalysedObject>(jsonResponse);
+
+                    if (analysedObject.tags == null)
+                    {
+                        Debug.Log("analysedObject.tagData is null");
+                    }
+                    else
+                    {
+                        Dictionary<string, float> tagsDictionary = new Dictionary<string, float>();
+
+                        foreach (TagData td in analysedObject.tags)
+                        {
+                            TagData tag = td as TagData;
+                            tagsDictionary.Add(tag.name, tag.confidence);
+                        }
+
+                        ResultsLabel.instance.SetTagsToLastLabel(tagsDictionary);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Debug.Log("Json exception.Message: " + exception.Message);
+                }
+
+                yield return null;
+            }
+
+            /// <summary>
+            /// Returns the contents of the specified file as a byte array.
+            /// </summary>
+            private static byte[] GetImageAsByteArray(string imageFilePath)
+            {
+                FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
     }
 }
