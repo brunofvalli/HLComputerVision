b35c0262dde9f8a1bfbb91a23f9af69f8e797aeb
Bruno <brunofvalli@gmail.com>
2018-10-03 08:24:33 -0500

    Starting the project
    Starting the project

---
+++ Assets/Scripts/VisionManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets.Scripts
+{
+    public class VisionManager : MonoBehaviour
+    {
+        [System.Serializable]
+        public class TagData
+        {
+            public string name;
+            public float confidence;
+        }
+
+        [System.Serializable]
+        public class AnalysedObject
+        {
+            public TagData[] tags;
+            public string requestId;
+            public object metadata;
+        }
+    }
+}
